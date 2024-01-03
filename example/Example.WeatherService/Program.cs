e62e2a40a6abf0d0136334f0e9f1254fcc84d916
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-01-03 13:11:30 +1000

    Better example, remove StartActivity<T> for now

---
+++ example/Example.WeatherService/Program.cs
@@ -0,0 +1,48 @@
+using Serilog;
+using Serilog.Templates.Themes;
+using SerilogTracing;
+using SerilogTracing.Formatting;
+// ReSharper disable RedundantSuppressNullableWarningExpression
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithProperty("Application", typeof(Program).Assembly.GetName().Name)
+    .WriteTo.Console(DefaultFormatting.CreateTextFormatter(TemplateTheme.Code))
+    .WriteTo.Seq(
+        "http://localhost:5341",
+        payloadFormatter: DefaultFormatting.CreateJsonFormatter(),
+        messageHandler: new SocketsHttpHandler { ActivityHeadersPropagator = null })
+    .CreateTracingLogger();
+
+Log.Information("Weather service starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    var forecastByPostcode = Directory.GetFiles("./data")
+        .ToDictionary(f => Path.GetFileNameWithoutExtension(f)!, f => File.ReadAllText(f).Trim());
+
+    app.MapGet("/{postcode}", (string postcode) =>
+    {
+        using var activity = Log.Logger.StartActivity("Look up forecast for postcode {Postcode}", postcode);
+        var forecast = forecastByPostcode[postcode];
+        activity.AddProperty("Forecast", forecast);
+        return forecast;
+    });
+
+    app.Run();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+    return 1;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
---
+++ example/weather/Program.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Templates.Themes;
+using SerilogTracing;
+using SerilogTracing.Formatting;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithProperty("Application", typeof(Program).Assembly.GetName().Name)
+    .WriteTo.Console(DefaultFormatting.CreateTextFormatter(TemplateTheme.Code))
+    .WriteTo.Seq(
+        "http://localhost:5341",
+        payloadFormatter: DefaultFormatting.CreateJsonFormatter(),
+        messageHandler: new SocketsHttpHandler { ActivityHeadersPropagator = null })
+    .CreateTracingLogger();
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Usage: weather <POSTCODE>");
+    return 1;
+}
+
+var postcode = args[0];
+
+using var activity = Log.Logger.StartActivity("Request weather for postcode {Postcode}", postcode);
+
+try
+{
+    var weatherClient = new HttpClient { BaseAddress = new("http://localhost:5133") };
+    var forecast = await weatherClient.GetStringAsync(postcode);
+    Console.WriteLine(forecast);
+
+    activity.Complete();
+    return 0;
+}
+catch (Exception ex)
+{
+    activity.Complete(LogEventLevel.Fatal, ex);
+    return 1;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
