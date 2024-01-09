305804d06d61ab368f15f2b5063d85af0fa752f6
Liam McLennan <liam.mclennan@gmail.com>
2024-01-09 16:36:23 +1000

    Port OTEL sink and add trace protobuf types

---
+++ test/Serilog.Sinks.OpenTelemetry.Tests/RequestTemplateFactoryTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2022 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using OpenTelemetry.Proto.Logs.V1;
+using Serilog.Sinks.OpenTelemetry.ProtocolHelpers;
+using Xunit;
+
+namespace Serilog.Sinks.OpenTelemetry.Tests;
+
+public class RequestTemplateFactoryTests
+{
+    [Fact]
+    public void ResourceLogsAreClonedDeeply()
+    {
+        var template = RequestTemplateFactory.CreateResourceLogs(new Dictionary<string, object>());
+
+        var clone = template.Clone();
+
+        var n = clone.ScopeLogs.Count;
+        Assert.Equal(0, n);
+
+        clone.ScopeLogs.Add(new ScopeLogs());
+
+        n = clone.ScopeLogs.Count;
+        Assert.Equal(1, n);
+        
+        clone = template.Clone();
+
+        n = clone.ScopeLogs.Count;
+        Assert.Equal(0, n);
+    }
+}
