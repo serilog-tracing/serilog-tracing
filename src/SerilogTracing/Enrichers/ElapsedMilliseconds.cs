7d16a370f0d22fe9335ce2ec8baf45abf3e077d6
KodrAus <kodraus@hey.com>
2024-02-01 13:12:39 +1000

    make enrichers internal

--- src/SerilogTracing/Enrichers/ElapsedMilliseconds.cs
+++ src/SerilogTracing/Enrichers/ElapsedMilliseconds.cs
@@ -1,20 +1,11 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Serilog.Core;
+﻿using Serilog.Core;
 using Serilog.Events;
 using SerilogTracing.Interop;
-using Constants = SerilogTracing.Core.Constants;
 
 namespace SerilogTracing.Enrichers;
 
-/// <summary>
-/// An enricher that adds the span duration as milliseconds.
-/// </summary>
-public class ElapsedMilliseconds: ILogEventEnricher
+internal class ElapsedMilliseconds: ILogEventEnricher
 {
-    /// <summary>
-    /// Construct an enricher that will add the span duration with the given property name. 
-    /// </summary>
-    /// <param name="propertyName">The name of the property to add with the span duration.</param>
     public ElapsedMilliseconds(string propertyName)
     {
         _propertyName = propertyName;
@@ -22,7 +13,6 @@ public class ElapsedMilliseconds: ILogEventEnricher
     
     readonly string _propertyName;
     
-    /// <inheritdoc cref="ILogEventEnricher.Enrich"/>
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
         if (logEvent.TryGetElapsed(out var elapsed))
--- src/SerilogTracing/Enrichers/ElapsedTime.cs
+++ src/SerilogTracing/Enrichers/ElapsedTime.cs
@@ -4,15 +4,8 @@ using SerilogTracing.Interop;
 
 namespace SerilogTracing.Enrichers;
 
-/// <summary>
-/// An enricher that adds the span duration as a <see cref="TimeSpan"/>.
-/// </summary>
-public class ElapsedTime: ILogEventEnricher
+internal class ElapsedTime: ILogEventEnricher
 {
-    /// <summary>
-    /// Construct an enricher that will add the span duration with the given property name. 
-    /// </summary>
-    /// <param name="propertyName">The name of the property to add with the span duration.</param>
     public ElapsedTime(string propertyName)
     {
         _propertyName = propertyName;
@@ -20,7 +13,6 @@ public class ElapsedTime: ILogEventEnricher
     
     readonly string _propertyName;
     
-    /// <inheritdoc cref="ILogEventEnricher.Enrich"/>
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
     {
         if (logEvent.TryGetElapsed(out var elapsed))
