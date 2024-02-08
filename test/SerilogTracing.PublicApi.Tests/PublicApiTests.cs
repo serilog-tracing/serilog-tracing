0360b5db2ec1696c7773ba15bc83b99b30e76bdf
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-02-08 12:42:16 +1000

    Public API surface approval tests

---
+++ test/SerilogTracing.PublicApi.Tests/PublicApiTests.cs
@@ -0,0 +1,26 @@
+﻿using PublicApiGenerator;
+using Shouldly;
+using Xunit;
+
+namespace SerilogTracing.PublicApi.Tests;
+
+public class PublicApiTests
+{
+    [Fact]
+    public void PublicApiSurfaceIsStable()
+    {
+        var assembly = typeof(ActivityListenerConfiguration).Assembly;
+        var publicApi = assembly.GeneratePublicApi(
+            new ApiGeneratorOptions
+            {
+                IncludeAssemblyAttributes = false,
+                ExcludeAttributes = ["System.Diagnostics.DebuggerDisplayAttribute"]
+            });
+
+        publicApi.ShouldMatchApproved(options =>
+        {
+            options.WithFilenameGenerator((_, _, fileType, fileExtension) =>
+                $"{assembly.GetName().Name!}.{fileType}.{fileExtension}");
+        });
+    }
+}
