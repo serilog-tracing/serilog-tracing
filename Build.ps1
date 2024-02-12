499d34961afd35e5a72c7e7b6e743f094c7afaed
KodrAus <kodraus@hey.com>
2024-02-12 10:53:23 +1000

    fix up quotes in build

--- Build.ps1
+++ Build.ps1
@@ -16,7 +16,7 @@ if(Test-Path .\artifacts) {
 
 $branch = @{ $true = $env:CI_TARGET_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:CI_TARGET_BRANCH];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:CI_BUILD_NUMBER];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace "([^a-zA-Z0-9\-]*)", "")-$revision"}[$branch -eq "main" -and $revision -ne "local"]
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"}[$branch -eq "main" -and $revision -ne "local"]
 
 Write-Output "build: Package version suffix is $suffix"
 
