35c2b279a3df427d4330fbbed15f2e983f433e79
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-05-01 12:35:32 +1000

    More docs

--- src/SerilogTracing.Instrumentation.SqlClient/Instrumentation/SqlClient/SqlCommandActivityInstrumentationOptions.cs
+++ src/SerilogTracing.Instrumentation.SqlClient/Instrumentation/SqlClient/SqlCommandActivityInstrumentationOptions.cs
@@ -66,6 +66,10 @@ public sealed class SqlCommandActivityInstrumentationOptions
     /// A function to populate properties on the activity from an executing <see cref="SqlCommand"/>. The callback
     /// runs before the command is executed.
     /// </summary>
+    /// <remarks>
+    /// When providing this setting, if the default properties are still required, keep a reference to the original
+    /// (default) property value and call it from within the new replacement callback.
+    /// </remarks>
     public Func<SqlCommand, IEnumerable<LogEventProperty>> GetCommandProperties { get; set; }
 
     /// <summary>
@@ -77,6 +81,7 @@ public sealed class SqlCommandActivityInstrumentationOptions
     /// <summary>
     /// Include the command text (i.e. the stored procedure name, SQL statement, or table name) in spans. The risk
     /// of exposing sensitive data, and bloating span size, means that the default value is <c langword="false"/>.
+    /// Ignored if <see cref="GetCommandProperties"/> is specified and does not chain calls to the default value.
     /// </summary>
     public bool IncludeCommandText { get; set; } = false;
 
@@ -84,6 +89,7 @@ public sealed class SqlCommandActivityInstrumentationOptions
     /// Attempt to infer the operation (<c>SELECT</c>, <c>INSERT</c>, <c>UPDATE</c>, <c>DELETE</c>, or <c>EXEC</c>)
     /// by inspecting the command text. The inferred value may be incorrect in some cases as only very limited command
     /// parsing is performed. The default is <c langword="true"/>.
+    /// Ignored if <see cref="GetCommandProperties"/> is specified and does not chain calls to the default value.
     /// </summary>
     public bool InferOperation { get; set; } = true;
 }
